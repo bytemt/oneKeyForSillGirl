df71474cfd80685607c06cdcb33474f040174d4d
hdbjlizhe <hdbjlizhe@hotmail.com>
2021-12-15 20:36:26 +0800

    Update sillyGirl.sh

--- sillyGirl.sh
+++ sillyGirl.sh
@@ -32,7 +32,7 @@ change()
         apt-get install wget -y
         url=https://golang.google.cn/dl/go1.17.5.linux-arm64.tar.gz
         wget $url
-        tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
+        tar -C /usr/local -xzf go1.17.5.linux-arm64.tar.gz
     elif [[ $i -eq 4 ]];then
         exit 1
     else
