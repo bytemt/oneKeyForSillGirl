e62b3ac1118b780e128de2ddc64ffc774c476123
hdbjlizhe <hdbjlizhe@hotmail.com>
2021-12-15 09:24:12 +0800

    Create dev.go

---
+++ dev.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	_ "github.com/cdle/sillyGirl/develop/qinglong"
+	_ "github.com/cdle/sillyGirl/im/qq"
+	_ "github.com/cdle/sillyGirl/im/tg"
+	_ "github.com/cdle/sillyGirl/im/wxmp"
+	_ "github.com/cdle/sillyGirl/im/wx"
+	//_ "github.com/cdle/sillyGirl/develop/jd_price"
+	_ "github.com/cdle/sillyGirl/develop/jd_cookie"
+  //_ "github.com/cdle/sillyGirl/develop/cable"
+  _ "github.com/cdle/sillyGirl/develop/fanli_jd"
+  _ "github.com/cdle/sillyGirl/develop/fanli_tb"
+//_ "github.com/cdle/sillyGirl/develop/onebyone"
+	_ "github.com/cdle/sillyGirl/develop/fanli_pdd"
+  _ "github.com/cdle/sillyGirl/develop/fanli_vip"
+)
