dbea2b7643de903b311a5d33b22f3c3d413cc90c
hdbjlizhe <hdbjlizhe@hotmail.com>
2021-12-15 15:47:55 +0800

    Add files via upload

--- sillyGirl.sh
+++ sillyGirl.sh
@@ -70,32 +70,40 @@ maingo()
 sillyGirl()
 {
     s=sillyGirl;#检查系统是arm版还是x86_64版
-    echo "检查系统是arm版还是x86_64版"
+    echo "检查系统是arm版还是x86_64版..."
     a=arm64;
     if [[ $(uname -a | grep "x86_64") != "" ]];
         then a=amd64;
     fi ;
     #如果当前系统存在sillyGirl，先杀死
-    echo "检查当前系统是否存在sillyGirl"
+    echo "检查当前系统是否有正在运行的sillyGirl..."
     pkill -9 $s;
     #检查当前目录有没有sillyGirl文件，如果有进入备份sillyGirl.cache文件
-
+    if [ ! -d "/sillyGirl/" ]; then
+        echo "当前存在sillyGirl目录,准备备份数据文件..."
+        cp sillyGirl/sillyGirl.cache ./
+        sleep 3s
+        echo "发现sillyGirl文件夹，删除中..."
+        rm -r sillyGirl
+        sleep 3s
+    fi
     #克隆sillyGirl库
-    echo "开始克隆sillyGirl库"
+    echo "开始克隆sillyGirl库..."
     git clone https://ghproxy.com/https://github.com/cdle/sillyGirl.git;
     #进入sillyGirl文件夹
-    if [[ $? == 0 ]]; then 
-        sleep 3s 
-        cd $s;
-        sleep 3s 
+    if [ $? -eq 0 ]; then 
+        sleep 10s
+        echo "进入sillyGirl文件夹..."
+        cd $s
     fi
-    echo "进入sillyGirl文件夹"
     #获取dev.go文件
     echo "下载dev.go文件"
     wget https://ghproxy.com/https://raw.githubusercontent.com/hdbjlizhe/oneKeyForSillGirl/main/dev.go
     #进入develop文件夹
-    if [[ $? == 0 ]]; then 
+    if [[ $? == 0 ]]; then
+        sleep 3s
         cd develop;
+        sleep 3s
     fi
     echo "进入develop文件夹，开始克隆常用库、返利库"
     #克隆一些必要的库
@@ -113,6 +121,13 @@ sillyGirl()
         go build;
         echo "编译完成！！！"
     fi
+    #将备份的文件拷进sillyGirl文件夹
+    sleep 3s
+    cd ..
+    echo "拷贝数据文件到sillyGirl目录"
+    cp ./sillyGirl.cache sillyGirl/;
+    sleep 3s
+    echo "安装完成，开始享用吧!!!"
 }
 
 if [ "$GOROOT" != "" ]; then
